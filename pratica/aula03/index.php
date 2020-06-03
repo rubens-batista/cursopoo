7a7d7e6ba32eb65ceb47dda152cd590b0f95bb32
Rubens Batista <nunobfc24@gmail.com>
2020-06-03 21:53:19 +0100

    add aula 06a

--- pratica/aula03/index.php
+++ pratica/aula03/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula02 - POO</title>
+    <title>Aula03 - POO</title>
 </head>
 <body>
     <pre>
---
+++ pratica/aula06/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula06 - POO</title>
+</head>
+<body>
+    <pre>
+        <?php
+            
+        ?>
+    </pre>
+</body>
+</html>
