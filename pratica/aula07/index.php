366c2c0bad5d77ec60b91611c2a05d20c9fd12db
Rubens Batista <nunobfc24@gmail.com>
2020-06-04 21:04:51 +0100

    add aula07a-b

---
+++ pratica/aula07/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula07 - POO</title>
+</head>
+<body>
+    <h1>Projeto Ultimate Emoji Combat</h1>
+    <pre>
+        <?php
+            require_once "Lutador.php";
+            $l = array();
+            $l[0] = new Lutador("Pretty Boy", "França", 30, 1.75, 68.9, 11, 2, 1);
+            $l[1] = new Lutador("PutScript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+            $l[2] = new Lutador("SnapShadow", "EUA", 35, 1.68, 80.9, 12, 2, 1);
+            $l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
+            $l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+            $l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+            
+            $l[3]->perderLuta();
+            $l[3]->apresentar();
+            $l[3]->status();
+
+        ?>
+    </pre>
+</body>
+</html>
